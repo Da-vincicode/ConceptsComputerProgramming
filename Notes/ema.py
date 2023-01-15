cdff137e7fb18dc2df7f94478c6170532df3a558
Da-vinciCode <jonathan.damien25@gmail.com>
2023-01-15 12:40:18 -0800

    Added file

---
+++ Notes/ema.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+print ("do i love my baby?")
+answerbox=input("yes,maybe or no \nanswer: ")
+r1="love you 2"
+r2= "think harder"
+r3= "fuck off"
+if answerbox == "yes":
+    print(r1)
+elif answerbox=="maybe":
+    print(r2)
+elif answerbox=="no":
+    print(r3)
+
+
+# In[ ]:
+
+
+
+
